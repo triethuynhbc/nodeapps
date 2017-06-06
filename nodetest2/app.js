b39a37b23ae0f0511c21a166e9a3677df96eee13
Prog219 - Spring 2017 - Triet Huynh <triethuynh05@outlook.com>
2017-06-05 22:25:21 -0700

    Add nodetest2 walkthrough app

---
+++ nodetest2/app.js
@@ -0,0 +1,93 @@
+//====================================================================================================
+// Walk through doc: http://cwbuecheler.com/web/tutorials/2014/restful-web-app-node-express-mongodb/
+//
+// BCC - Spring 2017 - PROG 219
+// Student: Triet Huynh
+//====================================================================================================
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+// Add for database
+// New code that knows mongo and a nice abstract layer
+var mongo = require('mongodb');
+var monk = require('monk');
+
+// Connection string to mlab
+//mongodb://<dbuser>:<dbpassword>@ds133251.mlab.com:33251/prog219
+var db = monk('mongodb://trietdbuser:trietdbuser123@ds133251.mlab.com:33251/prog219');
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// My favicon in folder "/public"
+app.use(favicon(path.join(__dirname, 'public', 'images/triethuynh32x32px.png')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router or in other words, to various http requests.
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stack traces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
